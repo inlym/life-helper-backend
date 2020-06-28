3171ba9e99a45bc96643f40f51fe8511c1fc32e3
inlym <inlym@qq.com>
2020-06-28 23:00:14 +0800

    更新了 getLocationByIP 函数

--- src/ext/ip.js
+++ src/ext/ip.js
@@ -16,53 +16,53 @@ const { IP_LOCATION_API_APPCODE } = require('../config/appcode')
  * 
  * 
  * @param {string} ipAddress IP地址
+ * @returns {Promise} 
+ * 
+ * 		返回数据格式演示：
+ * 			{
+ *				nation: '中国',
+ *				en_short: 'CN',
+ *				en_name: 'China',
+ *				province: '浙江省',
+ *				city: '杭州市',
+ *				district: '西湖区',
+ *				adcode: 330106,
+ *				lat: 30.12345,
+ *				lng: 120.12345
+ *			}
+ * 
  */
-async function getLocationByIP(ipAddress) {
-	if (!ipAddress) {
-		throw new Error('IP地址为空')
-	}
-
-	if (!net.isIPv4(ipAddress)) {
-		throw new Error('IP地址错误，不是一个正常的IPv4地址')
-	}
-
-	const response = await axios({
-		url: 'https://ips.market.alicloudapi.com/iplocaltion',
-		method: 'GET',
-		params: {
-			ip: ipAddress
-		},
-		headers: {
-			Authorization: 'APPCODE ' + IP_LOCATION_API_APPCODE
+function getLocationByIP(ipAddress) {
+	return new Promise(async function (resolve, reject) {
+		if (!ipAddress) {
+			reject(new Error('错误：IP地址为空'))
 		}
+
+		if (!net.isIPv4(ipAddress)) {
+			reject(new Error('错误：IP地址格式错误'))
+		}
+
+		const response = await axios({
+			url: 'https://ips.market.alicloudapi.com/iplocaltion',
+			method: 'GET',
+			params: {
+				ip: ipAddress
+			},
+			headers: {
+				Authorization: 'APPCODE ' + IP_LOCATION_API_APPCODE
+			}
+		})
+
+		if (response.status !== 200) {
+			reject(new Error('第三方错误：HTTP状态码非200'))
+		}
+
+		if (response.data.code !== 100) {
+			reject(new Error('第三方错误，错误原因：' + response.data.message))
+		}
+
+		resolve(response.data.result)
+
 	})
-
-	const result = response.data.result
-
-	/**
-	 * result 结构
-	 * {
-	 *  "nation": "中国",
-	 *  "en_short": "CN",
-	 *  "en_name": "China",
-	 *  "province": "四川省",
-	 *  "city": "绵阳市",
-	 *  "district": "涪城区",
-	 *  "adcode": 510703,
-	 *  "lat": 31.45498,
-	 *  "lng": 104.75708
-	 * }
-	 */
-
-	let obj = {}
-	obj.nation = result.nation
-	obj.province = result.province
-	obj.city = result.city
-	obj.district = result.district
-	obj.adcode = result.adcode
-	obj.lat = result.lat
-	obj.lng = result.lng
-
-	return obj
 }
 
