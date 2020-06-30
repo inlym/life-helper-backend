a58d53353e391e25b1c6821e5e5c63afc4623e94
inlym <inlym@qq.com>
2020-06-30 23:07:53 +0800

    更新配置文件，新增 OSS_IMG 配置

--- src/config/config.development.demo.js
+++ src/config/config.development.demo.js
@@ -5,6 +5,7 @@ const MYSQL_MAIN_CONFIG = {
 }
 
 
+
 const REDIS_MAIN_CONFIG = {
 	host:'xxxxxxxxxxxxxxxx',
 	port:6379,
@@ -14,7 +15,26 @@ const REDIS_MAIN_CONFIG = {
 }
 
 
+
+/**
+ * 阿里云 OSS 配置，详情见阿里云官方文档
+ * https://help.aliyun.com/document_detail/64097.html
+ * 
+ * [ OSS_IMG ] 专门用来存放图片文件
+ */
+const OSS_IMG_CONFIG = {
+	region:'oss-cn-hangzhou',
+	accessKeyId:'xxxxxxxxxxxxxxxx',
+	accessKeySecret:'xxxxxxxxxxxxxxxx',
+	bucket:'xxxxxxxxxxxxxxxx',
+	endpoint:'oss-cn-hangzhou.aliyuncs.com',
+	internal:false,    // 测试环境使用公网访问 OSS
+}
+
+
+
 module.exports = {
 	MYSQL_MAIN_CONFIG,
-	REDIS_MAIN_CONFIG
+	REDIS_MAIN_CONFIG,
+	OSS_IMG_CONFIG,
 }
