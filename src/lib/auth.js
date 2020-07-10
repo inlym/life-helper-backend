c0bceb19523fd84efa142028a99e249b16f48d9f
inlym <inlym@qq.com>
2020-07-10 21:05:34 +0800

    优化 getUserIdByToken 函数，补充处理查询的 token 不存在的情况

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,7 +12,7 @@ const { generateRandomString } = require('../utils/string')
 
 
 /**
- * 为指定用户ID生成 token, 并存入 Redis
+ * 为指定用户ID生成 token, 并存入 Redis (有效期 2 天)
  * 
  * 备注：
  * 1. 未检测重复性，可能同个 userId 存在多个 token, 目前允许这种情况存在
@@ -30,19 +30,20 @@ async function createTokenForSpecificUserId(userId) {
 
 	const redis = new Redis(REDIS_MAIN_CONFIG)
 
-	// 生成1个32位长度的随机字符创(未检测是否重复)用作token
+	// 生成 1 个 32 位长度的随机字符创(未检测是否重复)用作 token
 	const token = generateRandomString(32)
 
-	await redis.set('token:' + token, userId)
+	// 保存 token, 有效期 2 天
+	await redis.set('token:' + token, userId, 'EX', 3600 * 24 * 2)
 	await redis.quit()
-	
+
 	return Promise.resolve(token)
 }
 
 
 
 /**
- * 通过 token 获取用户ID
+ * 通过 token 获取用户ID (如果 token 不存在，则返回 -1)
  * 
  * [ Redis ] 
  * key   => token:[token]
@@ -52,18 +53,23 @@ async function createTokenForSpecificUserId(userId) {
  * @param {string} token 
  * @returns {Promise} resolve(userId)
  */
-async function getUserIdByToken(token){
+async function getUserIdByToken(token) {
 	if (!token || typeof token !== 'string') throw new Error('参数错误: token为空或非字符串')
 
 	const redis = new Redis(REDIS_MAIN_CONFIG)
 
-	let userId = await redis.get('token:'+token)
+	const result = await redis.get('token:' + token)
 	await redis.quit()
 
-	// 获取到的 userId 为 string, 转为 number
-	userId = parseInt(userId)
+	if (!result) {
+		// 如果查询的 token 不存在，则返回 -1
+		return Promise.resolve(-1)
+	} else {
+		// 获取到的 userId 为 string, 转为 number
+		userId = parseInt(result)
 
-	return Promise.resolve(userId)
+		return Promise.resolve(userId)
+	}
 }
 
 
