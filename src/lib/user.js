f6f8df6f88d81b7689a2d16462bb0e26d0e4165f
inlym <inlym@qq.com>
2020-07-13 19:46:21 +0800

    新增 getUserIdByOpenid 函数

--- src/lib/user.js
+++ src/lib/user.js
@@ -30,10 +30,10 @@ function registerNewWxUser(openid) {
 		const value = [openid]
 
 		mysql.query(sql, value, function (error, results, fields) {
-			if (error){
+			if (error) {
 				reject(error)
 				mysql.end()
-			}else{
+			} else {
 				const insertId = results.insertId
 				resolve(insertId)
 				mysql.end()
@@ -44,6 +44,45 @@ function registerNewWxUser(openid) {
 
 
 
+/**
+ * 通过 openid, 从 user 表中获取用户 ID (如果 openid 不存在，则返回 0)
+ * 
+ * [ Mysql ]
+ * table => user
+ * 
+ * @param {string} openid 
+ * @returns {Promise} resolve(userId: number)
+ */
+function getUserIdByOpenid(openid) {
+	if (!openid || typeof openid !== 'string') throw new Error('参数错误: openid 为空或非字符串')
+	return new Promise(function (resolve, reject) {
+		// Mysql 连接
+		const mysql = Mysql.createConnection(MYSQL_MAIN_CONFIG)
+		mysql.connect()
+
+		const sql = 'SELECT id FROM user where openid = ? LIMIT 1;'
+		const value = [openid]
+
+		mysql.query(sql, value, function (error, results, fields) {
+			if (error) {
+				reject(error)
+				mysql.end()
+			} else {
+				if (results.length === 0) {
+					resolve(0)
+				} else {
+					resolve(results[0]['id'])
+				}
+				mysql.end()
+			}
+		})
+	})
+
+}
+
+
+
 module.exports = {
 	registerNewWxUser,
+	getUserIdByOpenid,
 }
