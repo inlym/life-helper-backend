a2567fc7d9a7ef64cb99fbd1f0140a9ea4e89e4a
inlym <inlym@qq.com>
2020-07-28 19:55:58 +0800

    新增 mysql 封装函数

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,4 +1,24 @@
 'use strict'
 
-const Mysql = require('promise-mysql')
-const { MYSQL_MAIN_CONFIG, REDIS_MAIN_CONFIG } = require('../config/config')
+const Mysql = require('mysql')
+const { MYSQL_MAIN_CONFIG } = require('../config/config')
+
+const connection = Mysql.createConnection(MYSQL_MAIN_CONFIG)
+connection.connect()
+
+const mysqlP = {
+	query(sql, params) {
+		return new Promise(function (resolve, reject) {
+			connection.query(sql, params, function (error, results, fields) {
+				if(error){
+					reject(error)
+				}else{
+					resolve(results)
+				}
+			})
+		})
+	}
+}
+
+
+module.exports = mysqlP
