78536b5f2cb6c6d6763243af19e30655ede788a9
inlym <inlym@qq.com>
2020-08-06 20:59:28 +0800

    [feat] 新增 Controller => debug

---
+++ app/controller/debug.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { getLocationByIP } = require('../service/location.js')
+
+
+module.exports = async function debug(ctx, next) {
+	const request = {}
+	const client = {}
+
+	request.method = ctx.method
+	request.url = ctx.url
+	request.headers = ctx.headers
+	request.body = ctx.body
+	request.path = ctx.path
+	request.query = ctx.query
+	request.querystring = ctx.querystring
+
+	client.ip = ctx.ip
+	const location = await getLocationByIP(ctx.ip)
+	client.location = {
+		longitude: location.lng,
+		latitude: location.lat
+	}
+
+	ctx.body = {
+		request,
+		client,
+	}
+}
