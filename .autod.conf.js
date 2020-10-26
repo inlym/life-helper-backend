1656c91d41fb8a1fef0f54d6ee023e683d21738b
inlym <inlym@qq.com>
2020-10-26 18:01:31 +0800

    :construction:

--- .autod.conf.js
+++ .autod.conf.js
@@ -1,29 +1,19 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  write: true,
-  prefix: '^',
-  plugin: 'autod-egg',
-  test: [
-    'test',
-    'benchmark',
-  ],
-  dep: [
-    'egg',
-    'egg-scripts',
-  ],
-  devdep: [
-    'egg-ci',
-    'egg-bin',
-    'egg-mock',
-    'autod',
-    'autod-egg',
-    'eslint',
-    'eslint-config-egg',
-  ],
-  exclude: [
-    './test/fixtures',
-    './dist',
-  ],
-};
-
+	write: true,
+	prefix: '^',
+	plugin: 'autod-egg',
+	test: ['test', 'benchmark'],
+	dep: ['egg', 'egg-scripts'],
+	devdep: [
+		'egg-ci',
+		'egg-bin',
+		'egg-mock',
+		'autod',
+		'autod-egg',
+		'eslint',
+		'eslint-config-egg',
+	],
+	exclude: ['./test/fixtures', './dist'],
+}
