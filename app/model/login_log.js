0db1435fcba388d7371bd14df69630e2daa94c0a
inlym <inlym@qq.com>
2020-11-03 16:12:25 +0800

    :art: 调整了代码结构

---
+++ app/model/login_log.js
@@ -0,0 +1,101 @@
+'use strict'
+
+module.exports = (app) => {
+	const { DataTypes } = app.Sequelize
+
+	/**
+	 *  本模型的登录指：
+	 *  小程序端通过 code 换取服务端下发的 token 的行为
+	 *
+	 *  当前数据表仅用于后续统计用途，无业务关联
+	 */
+
+	/** 用户登录日志模型 */
+	const LoginLog = app.model.define(
+		'LoginLog',
+
+		{
+			user_id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				comment: '登录用户的 user_id',
+			},
+
+			login_time: {
+				type: DataTypes.DATE,
+				allowNull: false,
+				defaultValue: DataTypes.NOW,
+				comment: '登录时间',
+			},
+
+			code: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				comment: '小程序端拿到的 code',
+			},
+
+			token: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				comment: '服务端返回的 token',
+			},
+
+			ip: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				comment: '用户的 IP 地址',
+			},
+
+			nation: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: '',
+				comment: '国家',
+			},
+
+			province: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: '',
+				comment: '省份',
+			},
+
+			city: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: '',
+				comment: '城市',
+			},
+
+			district: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: '',
+				comment: '区县',
+			},
+
+			// 考虑到后期兼容性，使用字符串存储经纬度，使用时转换成浮点数
+			longitude: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: '',
+				comment: '经度',
+			},
+
+			latitude: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: '',
+				comment: '纬度',
+			},
+		},
+
+		{
+			tableName: 'login_log',
+			createdAt: false,
+			updatedAt: false,
+		}
+	)
+
+	return LoginLog
+}
---
+++ app/service/ip.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const { Service } = require('egg')
+
+class IpService extends Service {
+	/**
+	 *  从第三方接口通过 IP 地址换取定位信息（地区、经纬度等）
+	 *
+	 *  第三方API来源于阿里云云市场
+	 *  https://market.aliyun.com/products/57002002/cmapi00035184.html
+	 *
+	 *  返回数据格式演示：
+	 *  {"en_short":"CN","en_name":"China","nation":"中国","province":"浙江省","city":"杭州市","district":"西湖区","adcode":330106,"lat":30.25961,"lng":120.13026}
+	 * @param {string} ip
+	 */
+	async getLocation(ip) {
+		const { app } = this
+		const { ALIYUN_MARKET_API_APPCODE_IPLOCATION } = app.config
+
+		const url = 'https://ips.market.alicloudapi.com/iplocaltion'
+		const options = {
+			dataType: 'json',
+			method: 'GET',
+			data: {
+				ip,
+			},
+			headers: {
+				Authorization: `APPCODE ${ALIYUN_MARKET_API_APPCODE_IPLOCATION}`,
+			},
+		}
+
+		const response = await app.curl(url, options)
+
+		const SUCCESS_STATUS = 200
+		const SUCCESS_CODE = 100
+
+		if (response.status !== SUCCESS_STATUS) {
+			return Promise.reject(new Error('第三方错误：HTTP状态码非200'))
+		} else if (response.data.code !== SUCCESS_CODE) {
+			return Promise.reject(new Error(`第三方错误：错误原因：${response.data.message}`))
+		} else {
+			return response.data.result
+		}
+	}
+}
+
+module.exports = IpService
