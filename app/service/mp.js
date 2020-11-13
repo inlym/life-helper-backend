0cde6efea558040e9a6b4a925a33ee33cf63323e
inlym <inlym@qq.com>
2020-11-13 16:04:59 +0800

    :bulb: 修改 service/mp 的注释

--- app/service/mp.js
+++ app/service/mp.js
@@ -10,12 +10,7 @@ class MpService extends Service {
    * 通过 code 换取 session 信息
    * @see {@link https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html 微信小程序开发文档 }
    * @param {!string} code - 微信小程序端获取的临时登录凭证
-   * @returns {Promise<mpSession>} 微信请求返回的数据
-   *
-   * @typedef {Object} mpSession
-   * @property {string} session_key - 会话密钥
-   * @property {string} openid - 用户唯一标识
-   *
+   * @returns {Promise<{session_key:string;openid:string}>} 微信请求返回的数据
    * @example 返回内容样例:
    * {session_key:"xxxxxx",openid:"xxxxxx"}
    */
@@ -28,6 +23,11 @@ class MpService extends Service {
     return res.data
   }
 
+  /**
+   * 通过 code 换取 openid
+   * @param {!string} code - 微信小程序端获取的临时登录凭证
+   * @returns {Promise<string>}
+   */
   async code2Openid(code) {
     const { openid } = await this.code2Session(code)
     return openid
@@ -35,6 +35,7 @@ class MpService extends Service {
 
   /**
    * 获取小程序全局唯一后台接口调用凭据（access_token）
+   * @returns {Promise<{access_token:string;expires_in:number}>}
    */
   async getAccessToken() {
     const { appid, secret } = this.app.config.miniprogram
