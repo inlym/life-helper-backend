76b78fbf4569881be798b6aa1087690299f46944
inlym <inlym@qq.com>
2020-11-20 19:53:50 +0800

    :passport_control: 增加对 code 参数的校验

--- app/controller/login.js
+++ app/controller/login.js
@@ -4,7 +4,31 @@ const { Controller } = require('egg')
 
 class LoginController extends Controller {
   async wxLogin() {
-    const { code } = this.ctx.query
+    const { ctx, logger } = this
+
+    /** query 参数校验规则 */
+    const rule = {
+      code: {
+        required: true,
+        type: 'string',
+      },
+    }
+
+    // 对 query 参数进行校验
+    try {
+      ctx.validate(rule, ctx.query)
+    } catch (err) {
+      logger.debug(err)
+      ctx.body = {
+        errcode: 40001,
+        errmsg: 'code为空',
+      }
+      return
+    }
+
+    // 获取参数
+    const { code } = ctx.query
+
     const token = await this.service.auth.wxLogin(code)
     this.ctx.body = {
       token,
