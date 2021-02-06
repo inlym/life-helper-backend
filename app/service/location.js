efedf3550666929e5f77888709eaf5b7478c8005
inlym <inlym@qq.com>
2021-02-06 15:06:28 +0800

    :sparkles: service 新增逆地址解析函数，并完成测试用例

--- app/service/location.js
+++ app/service/location.js
@@ -3,6 +3,19 @@
 const { Service } = require('egg')
 
 class LocationService extends Service {
+  /** 为方便调用，额外增加该函数用于获取 腾讯位置服务 的开发密钥（key） */
+  TLKEY() {
+    const { TENCENT_LBS_KEYS } = this.app.config
+
+    /** TENCENT_LBS_KEYS 列表的长度 */
+    const keysLen = TENCENT_LBS_KEYS.length
+
+    // 从列表中随机抽取一个 key
+    const TENCENT_LBS_KEY = TENCENT_LBS_KEYS[Math.floor(Math.random() * keysLen)]
+
+    return TENCENT_LBS_KEY
+  }
+
   /**
    * 根据 IP 地址获取位置信息
    * @see https://lbs.qq.com/service/webService/webServiceGuide/webServiceIp
@@ -16,30 +29,87 @@ class LocationService extends Service {
       throw new Error('ip为空')
     }
 
-    const { app } = this
-    const { TENCENT_LBS_KEYS } = app.config
-
-    /** TENCENT_LBS_KEYS 列表的长度 */
-    const keysLen = TENCENT_LBS_KEYS.length
-
-    // 从列表中随机抽取一个 key
-    const TENCENT_LBS_KEY = TENCENT_LBS_KEYS[Math.floor(Math.random() * keysLen)]
+    const { app, logger } = this
 
     /** 请求参数 */
     const requestOptions = {
       url: 'https://apis.map.qq.com/ws/location/v1/ip',
       params: {
         ip,
-        key: TENCENT_LBS_KEY,
+        key: this.TLKEY(),
       },
     }
 
     const response = await app.axios(requestOptions)
     if (!response.data.status) {
+      logger.debug('[HTTP Request] [腾讯位置服务] [IP 定位] 接口请求成功')
       return response.data.result
     } else {
       throw new Error(`[HTTP Request] 接口请求失败，错误原因 => ${response.data.message}`)
     }
+  }
+
+  /**
+   * fetchLocationByIp(ip) 函数的缓存版
+   * @label [Redis] key => `ip@location:${ip}` / ex => 10days
+   * @since 2021-02-06
+   */
+  async getLocationByIp(...args) {
+    return this.ctx.service.cache.bindCache(
+      {
+        expiration: 60 * 60 * 24 * 10,
+        service: 'location',
+        fn: 'fetchLocationByIp',
+        keyPrefix: 'ip@location',
+      },
+      ...args
+    )
+  }
+
+  /**
+   * 根据经纬度坐标获取所在位置的文字描述
+   * @label [HTTP Request]
+   * @param {string|number} longitude 经度
+   * @param {string|number} latitude 纬度
+   * @since 2021-02-06
+   */
+  async fetchAddressByLocation(longitude, latitude) {
+    const { app, logger } = this
+
+    /** 请求参数 */
+    const requestOptions = {
+      url: 'https://apis.map.qq.com/ws/geocoder/v1',
+      params: {
+        location: `${latitude},${longitude}`,
+        get_poi: 0,
+        key: this.TLKEY(),
+      },
+    }
+
+    const response = await app.axios(requestOptions)
+    if (!response.data.status) {
+      logger.debug('[HTTP Request] [腾讯位置服务] [逆地址解析] 接口请求成功')
+      return response.data.result
+    } else {
+      throw new Error(`[HTTP Request] 接口请求失败，错误原因 => ${response.data.message}`)
+    }
+  }
+
+  /**
+   * fetchAddressByLocation(longitude, latitude) 函数的缓存版
+   * @label [Redis] key => `location@address:${longitude}/${latitude}` / ex => 2days
+   * @since 2021-02-06
+   */
+  async getAddressByLocation(...args) {
+    return this.ctx.service.cache.bindCache(
+      {
+        expiration: 60 * 60 * 24 * 2,
+        service: 'location',
+        fn: 'fetchAddressByLocation',
+        keyPrefix: 'location@address',
+      },
+      ...args
+    )
   }
 }
 
--- test/service/location.test.js
+++ test/service/location.test.js
@@ -2,14 +2,45 @@
 
 const { app, assert } = require('egg-mock/bootstrap')
 
-describe('service/location.js', () => {
-  it('[腾讯位置服务接口] IP 定位', async () => {
-    const ctx = app.mockContext()
+/** 测试用的 IP 地址 */
+const ip = '114.114.114.114'
 
-    // 测试用的 IP 地址
-    const ip = '114.114.114.114'
+/** 测试用的经纬度 */
+const longitude = 120.11111
+const latitude = 30.22222
+
+describe('service/location.js', () => {
+  it('[腾讯位置服务接口] [IP 定位] [HTTP Request]', async () => {
+    const ctx = app.mockContext()
 
     const res = await ctx.service.location.fetchLocationByIp(ip)
     assert(res.ip === ip)
+  })
+
+  it('[腾讯位置服务接口] [IP 定位] [Redis]', async () => {
+    const ctx = app.mockContext()
+
+    // 进行 2 次，保证至少有一次从 Redis 获取
+    const res1 = await ctx.service.location.getLocationByIp(ip)
+    const res2 = await ctx.service.location.getLocationByIp(ip)
+
+    assert(JSON.stringify(res1) === JSON.stringify(res2))
+  })
+
+  it('[腾讯位置服务] [逆地址解析] [HTTP Request]', async () => {
+    const ctx = app.mockContext()
+
+    const res = await ctx.service.location.fetchAddressByLocation(longitude, latitude)
+    assert(res.location.lat === latitude)
+  })
+
+  it('[腾讯位置服务] [逆地址解析] [Redis]', async () => {
+    const ctx = app.mockContext()
+
+    // 进行 2 次，保证至少有一次从 Redis 获取
+    const res1 = await ctx.service.location.getAddressByLocation(longitude, latitude)
+    const res2 = await ctx.service.location.getAddressByLocation(longitude, latitude)
+
+    assert(JSON.stringify(res1) === JSON.stringify(res2))
   })
 })
