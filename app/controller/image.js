bb6477b8b62569eb1f6a0cb694e087e777ad5a49
inlym <inlym@qq.com>
2021-03-02 15:27:50 +0800

    :bulb: 优化 uploadImage 控制器注释

--- app/controller/image.js
+++ app/controller/image.js
@@ -9,6 +9,18 @@ class ImageController extends Controller {
    * @apiName uploadImage
    * @apiGroup image
    * @apiVersion 0.1.0
+   * @apiDescription 用于客户端上传图片，本接口仅支持上传单张图片
+   *
+   * @apiParam (Body) {multipart/form-data}
+   *
+   * @apiSuccess (Response) {String} filename 文件名
+   * @apiSuccess (Response) {String} url 文件地址
+   *
+   * @apiSuccessExample {json} 返回值示例
+   * {
+   *   "filename":"62c383227c6a4eed87366b58d0d6c5b9.jpg",
+   *   "url":"https://img3rd.lh.inlym.com/62c383227c6a4eed87366b58d0d6c5b9.jpg"
+   * }
    */
   async upload() {
     const { ctx, app } = this
