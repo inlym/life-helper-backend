194871b87ecf7266d6e3ad1d4cbc2b4e5284f8e0
inlym <inlym@qq.com>
2021-03-06 20:43:47 +0800

    :sparkles: 新增 API 用于接收微信服务器的消息推送（目前用于测试，无实际功能）

--- app/router/wxserver.js
+++ app/router/wxserver.js
@@ -5,4 +5,5 @@ module.exports = (app) => {
 
   /** 用于接收由微信服务器发送的消息推送 */
   router.get('/wxserver/message', controller.wxserver.getMessagePush)
+  router.post('/wxserver/message', controller.wxserver.getMessagePush)
 }
