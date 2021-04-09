20f3d3f05960330ae7226023bb21044c3ce6f19b
inlym <inlym@qq.com>
2021-04-09 18:22:02 +0800

    :children_crossing: 优化记录请求日志中间件

--- app/middleware/requestLog.js
+++ app/middleware/requestLog.js
@@ -7,25 +7,24 @@
 
 module.exports = () => {
   return async function requestLog(ctx, next) {
-    await next()
-
     // '/ping' 接口用于健康检查，无需所任何记录
     if (ctx.path !== '/ping') {
       const { app, service } = ctx
+      const request = JSON.stringify(service.debug.getRequestDetail())
+
+      await next()
+
       const requestId = ctx.tracer.traceId
-
-      const message = {
-        request: service.debug.getRequestDetail(),
-        response: service.debug.getResponseDetail(),
-      }
-
-      const data = JSON.stringify(message)
+      const response = JSON.stringify(service.debug.getResponseDetail())
+      const data = JSON.stringify({ request, response })
 
       const { key: redisKey, timeout } = ctx.service.keys.requestLog(requestId)
       app.redis.set(redisKey, data, 'EX', timeout)
 
       const oss = app.oss.get('dataflow')
       oss.put('/request-log/' + requestId + '.json', Buffer.from(data))
+    } else {
+      await next()
     }
   }
 }
