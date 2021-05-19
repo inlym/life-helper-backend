14e9f7d646a1ed165c3a449968598c4f48d6c701
inlym <inlym@qq.com>
2021-05-19 14:24:44 +0800

    :heavy_plus_sign: 引入 `TypeORM`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DebugModule } from './modules/debug/debug.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { DebugModule } from './modules/debug/debug.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import getConfig from 'life-helper-config'
 
 @Module({
-  imports: [DebugModule],
+  imports: [TypeOrmModule.forRoot(getConfig('TypeOrm')), DebugModule],
   controllers: [AppController],
   providers: [AppService],
 })
