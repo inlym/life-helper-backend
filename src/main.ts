8dc15ac1f83862d6395a9f88300ce060649c00c9
inlym <inlym@qq.com>
2021-05-19 11:54:36 +0800

    :construction_worker: 使用 `Swagger` 插件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { setupSwagger } from './plugins/swagger.plugin'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  /** 挂载 Swagger 插件 */
+  setupSwagger(app)
+
+  await app.listen(3000, '0.0.0.0')
 }
-bootstrap();
+
+bootstrap()
