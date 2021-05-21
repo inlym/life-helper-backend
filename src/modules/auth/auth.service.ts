01bb346c38bc3542653f00adfba132c9a2b1e63a
inlym <inlym@qq.com>
2021-05-21 13:09:46 +0800

    :sparkles: 完成鉴权模块（`auth`）逻辑和测试用例

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,20 +7,23 @@ export class AuthService {
   constructor(private redisService: RedisService) {}
 
   /**
-   * 为指定用户 ID 生成 token，并存入 Redis
+   * 为指定用户生成登录凭证 token
+   * @param userId {number} 用户ID
+   * @param expiration {number} 有效期（单位：秒），默认 2 天
    */
-  async createToken(userId: number): Promise<string> {
-    /** 有效期：2天 */
-    const expiration = 3600 * 24 * 2
+  async createToken(userId: number, expiration: number = 3600 * 24 * 2): Promise<string> {
+    if (userId < 1) {
+      throw new Error('userId 不允许小于 1')
+    }
 
     /** `token` 是去掉短横线的 UUID 值 */
     const token: string = uuidv4().replace(/-/gu, '')
 
     /** 存入 Redis 的键名 */
-    const redisKey = `user:id:token:${token}`
+    const redisKey = `auth:userid:token:${token}`
 
-    /** 获取 Redis 实例 */
-    const redis = await this.redisService.getClient()
+    // 获取 Redis 实例
+    const redis = this.redisService.getClient()
 
     const result = await redis.set(redisKey, userId, 'EX', expiration)
     if (result === 'OK') {
@@ -28,5 +31,25 @@ export class AuthService {
     } else {
       throw new Error('调用 `redis.set` 方法时发生异常')
     }
+  }
+
+  /**
+   * 通过用户登录凭证换取用户 ID
+   * @param token {string} 用户登录凭证
+   *
+   * 1. 如果 token 无效则返回 `0`
+   */
+  async getUserIdByToken(token: string): Promise<number> {
+    /** Redis 的键名 */
+    const redisKey = `auth:userid:token:${token}`
+
+    // 获取 Redis 实例
+    const redis = this.redisService.getClient()
+
+    const result = await redis.get(redisKey)
+    if (result) {
+      return parseInt(result, 10)
+    }
+    return 0
   }
 }
