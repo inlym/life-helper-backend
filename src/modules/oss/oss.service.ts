6383ca68921eb4351107c0785a01cbf0d769dda9
inlym <inlym@qq.com>
2021-06-20 21:48:20 +0800

    :fire: 删除大量重构前使用的遗留文件

--- src/modules/oss/oss.service.ts
+++ src/modules/oss/oss.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import * as crypto from 'crypto'
 import { v4 as uuidv4 } from 'uuid'
+import { RedisService } from 'nestjs-redis'
 import { AliyunOssConfig } from 'life-helper-config'
+import axios from 'axios'
 
 @Injectable()
 export class OssService {
+  constructor(private redisService: RedisService) {
+    this.redis = this.redisService.getClient()
+  }
   /**
    * 生成用于客户端直传 OSS 所需的凭证信息
    * @param dirname {string} 目录名称
@@ -48,6 +53,72 @@ export class OssService {
       policy,
       OSSAccessKeyId: ugcBucket.accessKeyId,
       signature,
+    }
+  }
+
+  /**
+   * 通过检验签名验证回调请求由 OSS 发起
+   * @see https://help.aliyun.com/document_detail/31989.html#title-neu-ft5-rlp
+   *
+   * 说明：
+   * 1. 请求头 `x-oss-pub-key-url` - 获取公钥的 URL 地址
+   * 2. 请求头 `authorization` - 签名
+   */
+  async verifyOssCallbackSignature(signature: string, ossPubKeyUrlBase64: string) {
+    const { app, ctx, service } = this
+
+    /**
+     * 第 1 步：获取公钥
+     * 1. 获取后会放 Redis 存着，理论上猜测应该不常变。
+     * 2. 意外处理：如果出现异常则将公钥删除重新获取。
+     */
+    let pubKey = ''
+    const redisKey = 'system:oss-public-key'
+    const redisResult = await this.redis.get(redisKey)
+    if (redisResult) {
+      pubKey = redisResult
+    } else {
+      const ossPubKeyUrl = Buffer.from(ossPubKeyUrlBase64, 'base64').toString()
+
+      // 校验获取公钥的地址的 host 为 gosspublic.alicdn.com
+      const validHost = 'gosspublic.alicdn.com'
+      if (new URL(ossPubKeyUrl).host !== validHost) {
+        throw new Error(`OSS 回调异常：获取公钥的地址 ${ossPubKeyUrl} 非指定地址，可能为假冒请求！`)
+      }
+
+      const { data: resData } = await axios(ossPubKeyUrl)
+      if (resData) {
+        pubKey = resData
+        await redis.set(redisKey, resData, 'EX', 3600 * 24 * 10)
+      } else {
+        throw new Error('OSS 回调异常：未获取到公钥内容！')
+      }
+    }
+
+    /**
+     * 2021-06-20
+     * todo
+     * 以下内容待调整，复制于旧代码
+     */
+
+    // 计算签名字符串
+    const stringToSign = ctx.path + ctx.search + '\n' + ctx.request.rawBody
+
+    /** 签名内容 */
+    const signature = Buffer.from(ctx.get(HEADER_SIGNATURE), 'base64')
+
+    // 进行校验
+    const verifyResult = crypto.createVerify('RSA-MD5').update(stringToSign).verify(pubKey, signature)
+
+    this.logger.debug('verifyResult', verifyResult)
+
+    if (verifyResult) {
+      return true
+    } else {
+      // 存储错误回调
+      const { key: redisKey2 } = service.keys.ossErrorCallbackRequest(ctx.tracer.traceId)
+      app.redis.set(redisKey2, JSON.stringify(ctx.request))
+      throw new Error('OSS 回调异常：签名校验未通过')
     }
   }
 }
