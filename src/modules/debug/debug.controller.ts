bea4cc9157aa0960ddb9bd5f0433f30926f0fe3c
inlym <inlym@qq.com>
2021-07-09 14:30:23 +0800

    :fire: 移除了调试模块(`debug`) 中用于临时测试的代码

--- src/modules/debug/debug.controller.ts
+++ src/modules/debug/debug.controller.ts
@@ -1,22 +1,14 @@
-import { Body, Controller, Get, HttpException, Logger, Post, Query, Req } from '@nestjs/common'
+import { Body, Controller, Get, Logger, Post, Req } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { Request } from 'express'
 import { RedisService } from 'nestjs-redis'
-import { QueryId } from 'src/common/common.dto'
-import { User } from 'src/common/user.decorator'
-import { DebugItemService } from './debug-item.service'
-import { DebugProjectService } from './debug-project.service'
 
 @ApiTags('debug')
 @Controller('debug')
 export class DebugController {
   private readonly logger = new Logger(DebugController.name)
 
-  constructor(
-    private readonly redisService: RedisService,
-    private readonly debugProjectService: DebugProjectService,
-    private readonly debugItemService: DebugItemService
-  ) {}
+  constructor(private readonly redisService: RedisService) {}
 
   /**
    * 原样返回请求内容
@@ -63,54 +55,9 @@ export class DebugController {
     return { ...env, ...processInfo }
   }
 
-  @Get('user')
-  getUser(@User() user) {
-    return user
-  }
-
-  @Get('userid')
-  getUserId(@User('id') userId: number) {
-    return userId
-  }
-
-  @Get('test')
-  test() {
-    throw new Error('自定义错误')
-  }
-
-  @Get('test2')
-  test2() {
-    throw new HttpException({ name: 'mark' }, 502)
-  }
-
-  @Get('query')
-  query(@Query('id') id: number) {
-    return { id: id, type: typeof id }
-  }
-
   /** 测试日志 */
   @Post('logger')
   showLogger(@Body() body: any) {
     this.logger.verbose(body.content)
-  }
-
-  /** --- 下述控制器方法调试数据库的增删改查 --- */
-
-  @Get('projects')
-  async getAllProject(@User('id') userId: number) {
-    const list = await this.debugProjectService.getAll(userId)
-    return { list }
-  }
-
-  @Post('project')
-  async createProject() {
-    return this.debugProjectService.mockCreate()
-  }
-
-  @Get('project')
-  async getOne(@User('id') userId: number, @Query() query: QueryId) {
-    const { id } = query
-
-    return this.debugProjectService.getOne(userId, id)
   }
 }
--- src/modules/debug/debug.module.ts
+++ src/modules/debug/debug.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { DebugItem } from './debug-item.entity'
-import { DebugItemService } from './debug-item.service'
-import { DebugProject } from './debug-project.entity'
-import { DebugProjectService } from './debug-project.service'
 import { DebugController } from './debug.controller'
 
 /**
  * 当前模块用于处理调试相关逻辑
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([DebugProject, DebugItem])],
   controllers: [DebugController],
-  providers: [DebugProjectService, DebugItemService],
 })
 export class DebugModule {}
