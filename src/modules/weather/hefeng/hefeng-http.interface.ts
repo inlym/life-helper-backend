88301c2f9910f16c98cae9403c5373ade8eb2c37
inlym <inlym@qq.com>
2021-08-26 09:59:59 +0000

    :zap: 新增一些方法

--- src/modules/weather/hefeng/hefeng-http.interface.ts
+++ src/modules/weather/hefeng/hefeng-http.interface.ts
@@ -4,10 +4,19 @@ export interface HefengResponse {
   code: string
 
   /** 城市信息查询 */
-  location?: CityInfo[]
+  location: CityInfo[]
 
   /** 热门城市查询 */
-  topCityList?: CityInfo[]
+  topCityList: CityInfo[]
+
+  /** 实时天气数据 */
+  now: WeatherNow
+
+  /** 逐天天气预报 */
+  dayly: DailyForecastItem[]
+
+  /** 逐小时天气预报 */
+  hourly: HourlyForecastItem[]
 }
 
 /** 和风天气接口响应数据 - 城市信息查询 */
@@ -57,4 +66,194 @@ export interface CityInfo {
 
   /** 地区评分 */
   rank: string
+}
+
+/**
+ * 实时天气数据
+ *
+ * @see [实时天气](https://dev.qweather.com/docs/api/weather/weather-now/)
+ */
+export class WeatherNow {
+  /** 数据观测时间 */
+  obsTime: string
+
+  /** 温度，默认单位：摄氏度 */
+  temp: string
+
+  /** 体感温度，默认单位：摄氏度 */
+  feelsLike: string
+
+  /** 天气状况和图标的代码 */
+  icon: string
+
+  /** 天气状况的文字描述，包括阴晴雨雪等天气状态的描述 */
+  text: string
+
+  /** 风向360角度 */
+  wind360: string
+
+  /** 风向 */
+  windDir: string
+
+  /** 风力等级 */
+  windScale: string
+
+  /** 风速，公里/小时 */
+  windSpeed: string
+
+  /** 相对湿度，百分比数值 */
+  humidity: string
+
+  /** 当前小时累计降水量，默认单位：毫米 */
+  precip: string
+
+  /** 大气压强，默认单位：百帕 */
+  pressure: string
+
+  /** 能见度，默认单位：公里 */
+  vis: string
+
+  /** 云量，百分比数值 */
+  cloud: string
+
+  /** 露点温度 */
+  dew: string
+}
+
+/**
+ * 逐天天气预报
+ *
+ * @see [逐天天气预报](https://dev.qweather.com/docs/api/weather/weather-daily-forecast/)
+ */
+export class DailyForecastItem {
+  // 以下为原始数据（未做处理）
+
+  /** 预报日期 */
+  date: string
+
+  fxDate: string
+
+  /** 日出时间 */
+  sunrise: string
+
+  /** 日落时间 */
+  sunset: string
+
+  /** 月升时间 */
+  moonrise: string
+
+  /** 月落时间 */
+  moonset: string
+
+  /** 月相名称 */
+  moonPhase: string
+
+  /** 最高温度 */
+  tempMax: string
+
+  /** 最低温度 */
+  tempMin: string
+
+  /** 白天天气状况的图标代码 */
+  iconDay: string
+
+  /** 白天天气状况文字描述 */
+  textDay: string
+
+  /** 夜间天气状况的图标代码 */
+  iconNight: string
+
+  /** 夜间天气状况文字描述 */
+  textNight: string
+
+  /** 白天风向360角度 */
+  wind360Day: string
+
+  /** 白天风向 */
+  windDirDay: string
+
+  /** 白天风力等级 */
+  windScaleDay: string
+
+  /** 白天风速，公里/小时 */
+  windSpeedDay: string
+
+  /** 夜间风向360角度 */
+  wind360Night: string
+
+  /** 夜间当天风向 */
+  windDirNight: string
+
+  /** 夜间风力等级 */
+  windScaleNight: string
+
+  /** 夜间风速，公里/小时 */
+  windSpeedNight: string
+
+  /** 当天总降水量，默认单位：毫米 */
+  precip: string
+
+  /** 紫外线强度指数 */
+  uvIndex: string
+
+  /** 相对湿度，百分比数值 */
+  humidity: string
+
+  /** 大气压强，默认单位：百帕 */
+  pressure: string
+
+  /** 能见度，默认单位：公里 */
+  vis: string
+
+  /** 云量，百分比数值 */
+  cloud: string
+}
+
+/**
+ * 逐小时天气预报
+ *
+ * @see [逐小时天气预报](https://dev.qweather.com/docs/api/weather/weather-hourly-forecast/)
+ */
+export class HourlyForecastItem {
+  /** 预报时间 */
+  fxTime: string
+
+  /** 温度，默认单位：摄氏度 */
+  temp: string
+
+  /** 天气状况和图标的代码 */
+  icon: string
+
+  /** 天气状况的文字描述 */
+  text: string
+
+  /** 风向360角度 */
+  wind360: string
+
+  /** 风向 */
+  windDir: string
+
+  /** 风力等级 */
+  windScale: string
+
+  /** 风速，公里/小时 */
+  windSpeed: string
+
+  /** 相对湿度，百分比数值 */
+  humidity: string
+
+  /** 当前小时累计降水量，默认单位：毫米 */
+  precip: string
+
+  /** 逐小时预报降水概率，百分比数值，可能为空 */
+  pop: string
+
+  /** 大气压强，默认单位：百帕 */
+  pressure: string
+
+  /** 云量，百分比数值 */
+  cloud: string
+
+  /** 露点温度 */
+  dew: string
 }
--- src/modules/weather/hefeng/hefeng-http.service.ts
+++ src/modules/weather/hefeng/hefeng-http.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common'
 import axios from 'axios'
 import { HefengConfig } from 'life-helper-config'
 import { COMMON_SERVER_ERROR } from 'src/common/errors.constant'
-import { CityInfo, HefengResponse } from './hefeng-http.interface'
+import { CityInfo, HefengResponse, WeatherNow } from './hefeng-http.interface'
 
 /**
  * ### 模块说明
@@ -11,6 +11,16 @@ import { CityInfo, HefengResponse } from './hefeng-http.interface'
  * 1. 封装对 [和风天气](https://dev.qweather.com/docs/api/) API 的请求。
  * 2. 仅将请求参数封装为函数，不做缓存处理。
  * 3. 仅返回响应数据中的有效数据部分，不是将整个响应都返回的。
+ * ```
+ *
+ *
+ * ### 注意事项
+ *
+ * ```markdown
+ * 1. 各个请求的 `请求参数` 大同小异，可以封装成 `type -> apiInfo` 的形式去请求接口，只需要一个函数就够了，
+ * 而不是每一个对应的 API 都使用了一个函数，但千万不要这么做。
+ * 2. 上述做法的缺点是都写在一个函数内，里面需要有各种冗余的判断，会被限制住。
+ * 3. 当前版本就是从上述的版本改过来的，不要再重蹈覆辙了（宁可每个方法都有很多重复的代码），这样一劳永逸。
  * ```
  */
 @Injectable()
@@ -27,9 +37,9 @@ export class HefengHttpService {
    *
    * @description
    *
-   * ```markdown
-   * #### `location` 支持的形式：
+   * ### `location` 支持的形式：
    *
+   * ```markdown
    * 1. 查询地区的名称，支持模糊搜索。例如 `西安`。
    * 2. 以英文逗号分隔的经度,纬度坐标（十进制），例如 `116.41,39.92`。
    * 3. 和风天气内部定义的 `LocationID`。
@@ -97,6 +107,103 @@ export class HefengHttpService {
     } else {
       // 失败情况
       this.logger.error(`[接口请求错误] 和风天气 - 热门城市查询, 响应 code => \`${response.data.code}\` `)
+
+      throw new HttpException(COMMON_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  /**
+   * 获取实时天气数据
+   *
+   * @param location 需要查询地区的 `LocationID` 或以英文逗号分隔的 `经度,纬度` 坐标（十进制）
+   *
+   * @see [实时天气](https://dev.qweather.com/docs/api/weather/weather-now/)
+   */
+  async getWeatherNow(location: string): Promise<WeatherNow> {
+    const { key, baseURL } = HefengConfig.basic
+
+    const response = await axios.request<HefengResponse>({
+      baseURL,
+      url: '/weather/now',
+      params: {
+        key,
+        location,
+      },
+    })
+
+    if (response.data.code === '200') {
+      // `code` 为 `200` 表示请求成功
+      return response.data.now
+    } else {
+      // 失败情况
+      this.logger.error(`[接口请求错误] 和风天气 - 实时天气, 响应 code => \`${response.data.code}\`,  location => \`${location}\` `)
+
+      throw new HttpException(COMMON_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  /**
+   * 获取逐天天气预报
+   *
+   * @param location 需要查询地区的 `LocationID` 或以英文逗号分隔的 `经度,纬度` 坐标（十进制）
+   * @param days 天数
+   *
+   * @see [逐天天气预报](https://dev.qweather.com/docs/api/weather/weather-daily-forecast/)
+   */
+  async getDailyForecast(location: string, days: 3 | 7 | 10 | 15) {
+    /**
+     * 说明：
+     * 1. 3 天和 7 天使用开发版。
+     * 2. 10 天 和 15 天使用商业版。
+     */
+    const { key, baseURL } = [10, 15].includes(days) ? HefengConfig.pro : HefengConfig.basic
+
+    const response = await axios.request<HefengResponse>({
+      baseURL,
+      url: `/weather/${days}d`,
+      params: {
+        key,
+        location,
+      },
+    })
+
+    if (response.data.code === '200') {
+      // `code` 为 `200` 表示请求成功
+      return response.data.dayly
+    } else {
+      // 失败情况
+      this.logger.error(`[接口请求错误] 和风天气 - 逐天天气预报, 响应 code => \`${response.data.code}\`,  location => \`${location}\` `)
+
+      throw new HttpException(COMMON_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  /**
+   * 获取逐小时天气预报
+   *
+   * @param location 需要查询地区的 `LocationID` 或以英文逗号分隔的 `经度,纬度` 坐标（十进制）
+   * @param hours 小时数
+   *
+   * @see [逐小时天气预报](https://dev.qweather.com/docs/api/weather/weather-hourly-forecast/)
+   */
+  async getHourlyForecast(location: string, hours: 24 | 72 | 168) {
+    const { key, baseURL } = hours === 24 ? HefengConfig.basic : HefengConfig.pro
+
+    const response = await axios.request<HefengResponse>({
+      baseURL,
+      url: `/weather/${hours}h`,
+      params: {
+        key,
+        location,
+      },
+    })
+
+    if (response.data.code === '200') {
+      // `code` 为 `200` 表示请求成功
+      return response.data.hourly
+    } else {
+      // 失败情况
+      this.logger.error(`[接口请求错误] 和风天气 - 逐小时天气预报, 响应 code => \`${response.data.code}\`,  location => \`${location}\` `)
 
       throw new HttpException(COMMON_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR)
     }
