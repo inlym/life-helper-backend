1e4fe43ac8f96950409fb90e5664cc498935eb45
inlym <inlym@qq.com>
2021-08-26 08:24:15 +0000

    :wrench: 修改 `Swagger` 配置，使用路径改为 `/docs`

--- src/common/swagger.plugin.ts
+++ src/common/swagger.plugin.ts
@@ -2,11 +2,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { INestApplication } from '@nestjs/common'
 
 export function setupSwagger(app: INestApplication) {
-  const title = 'Life Helper Api Document'
-  const description = '我的个人助手项目接口文档'
+  const title = '「我的个人助手」项目 API 文档'
+  const description = '「我的个人助手」项目 API 文档，'
   const version = '1.0'
 
   const config = new DocumentBuilder().setTitle(title).setDescription(description).setVersion(version).build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('docs', app, document)
 }
