d8746d12e5bd0b40b10e46fe74f2d9d5f9793df7
inlym <inlym@qq.com>
2021-09-02 14:33:48 +0000

    :bulb: 优化全局异常过滤器 `AllExceptionFilter` 的说明

--- src/common/all-exception.filter.ts
+++ src/common/all-exception.filter.ts
@@ -1,21 +1,34 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common'
 import { Request, Response } from 'express'
 import { COMMON_SERVER_ERROR } from './errors.constant'
+
 /**
- * `@Catch()` 参数为空表示捕获所有错误
+ * 全局异常过滤器
+ *
+ *
+ * ## 说明
+ *
+ * ```markdown
+ * 1. 装饰器 `@Catch()` 参数为空表示捕获所有错误。
+ * ```
+ *
+ *
+ * ### 处理策略
+ *
+ * ```markdown
+ * 1. 继承自 `HttpException` 的错误，为已知错误，直接返回定义的结果。
+ * 2. 非继承自 `HttpException` 的错误，为未知错误，返回默认的错误提示。
+ * ```
  */
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(AllExceptionFilter.name)
 
-  /**
-   * 处理策略：
-   * 已知错误（继承自 `HttpException` 的错误）直接返回定义的结果，未知错误则返回固定的默认错误提示
-   */
-  catch(exception: HttpException | Error, host: ArgumentsHost) {
+  catch(exception: HttpException | Error, host: ArgumentsHost): Promise<void> {
     const ctx = host.switchToHttp()
-    const response = ctx.getResponse<Response>()
+
     const request = ctx.getRequest<Request>()
+    const response = ctx.getResponse<Response>()
 
     if (exception instanceof HttpException) {
       const status = exception.getStatus()
